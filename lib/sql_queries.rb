81c3b81a6311bc7809338a49bce57cf00ac4190d
Rebecca Tong <rtong@buffalo.edu>
2018-03-17 02:54:38 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -27,7 +27,7 @@ GROUP BY users.name ORDER BY SUM(pledges.amount) ASC;"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"SELECT projects.category, SUM(pledge.amount) FROM projects WHERE category = 'music';"
+"SELECT projects.category, SUM(pledges.amount) FROM projects WHERE category = 'music';"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
