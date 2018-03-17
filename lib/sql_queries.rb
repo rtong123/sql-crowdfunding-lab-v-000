42f83c7ef6626201ca0d2c059ee9440661231d1f
Rebecca Tong <rtong@buffalo.edu>
2018-03-17 02:29:04 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -22,7 +22,7 @@ end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
 "SELECT users.name, SUM(pledges.amount) FROM users JOIN pledges ON pledges.user_id = users.id
-GROUP BY user_id ORDER BY pledges.amount ASC;"
+GROUP BY users.name ORDER BY pledges.amount ASC;"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
